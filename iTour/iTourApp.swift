b6a2d0f2527ccbf04c75ceffec0b2c47a90b9c24
Philipp <pd95@users.noreply.github.com>
2023-10-08 13:59:39 +0200

    Initial Commit

---
+++ iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by Philipp on 08.10.23.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
